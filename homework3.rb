9845f1cd47e012f187aaf4b73bfc959b6c4300ae
Cecilly Deorocki <cdeorocki@live.com>
2015-12-13 19:51:29 -0500

    second commit, finished homeowork3

--- homework3.rb
+++ homework3.rb
@@ -1,10 +1,57 @@
 class Utility
-	attr_accessor :statement
+	
+	def count_words(string)
 
-		def count_words(string)
-			new_hash = 
-		end	
+		words = string.split(/\W+/)
+		
+		counts = Hash.new 0
+		
+		words.each do |word|
+		counts[word] += 1
+		end
+
+		counts
+
+	end
+
 end
 
-Utility.count_words("It was the best of times, it was the worst of times.")
+result = Utility.new
+
+puts result.count_words("It was the best of times, it was the worst of times.")
+
+
+class Animal
+	def speak
+		puts "Roar!"
+	end
+
+	def drink
+		puts "Slurp"
+	end
+end	
+	
+class Cow <	Animal
+	def speak
+	puts "Moooooo"
+	end	
+end	
+
+A = Animal.new
+C = Cow.new
+
+A.speak
+A.drink
+C.speak
+C.drink
+
+
+# Get and Post are both commonly used request-response methods between 
+# clients and servers. The Get method requests data from a specified
+# resource and provides information about the resource, indicated by the
+# URL. The Post method submits data to be processed to a specified
+# resource and makes changes related to the resource (generally making
+# a new one). Get provides resource information wehereas Post changes or
+# renews a resource.
+
 
